33c92f3334d1677cde723935c37ad30aa75ae28c
Wesley Toledo <wesley.toledo.hexatech@outlook.com>
2018-08-15 10:37:23 -0300

    Adicionando notificações e mexendo no nav lateral de navegação

--- index/assets/js/demo.js
+++ index/assets/js/demo.js
@@ -248,16 +248,15 @@ demo = {
         marker.setMap(map);
     },
 
-    showNotification: function(from, align) {
-        color = Math.floor((Math.random() * 4) + 1);
+    showNotification: function(from, align,messenger,tipo) {
 
         $.notify({
             icon: "notifications",
-            message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+            message: messenger
 
         }, {
-            type: type[color],
-            timer: 4000,
+            type: tipo,
+            timer: 1000,
             placement: {
                 from: from,
                 align: align
